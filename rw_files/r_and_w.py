163a606400085c05122b3230f6b3cbf46b5c9eee
borodinsky <borodinsky.v@gmail.com>
2024-10-12 21:15:48 +0300

    RW

--- rw_files/r_and_w.py
+++ rw_files/r_and_w.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # print(str(Path('spam', 'bacon', 'eggs')))
 
@@ -6,6 +7,28 @@ from pathlib import Path
 # for filename in my_files:
 #     print(Path(r'C:/Users/Al', filename))
 
-print(Path('spam') / 'bacon' / 'eggs')
-print(Path('spam') / Path('bacon/eggs'))
-print(Path('spam') / Path('bacon', 'eggs'))
+# print(Path('spam') / 'bacon' / 'eggs')
+# print(Path('spam') / Path('bacon/eggs'))
+# print(Path('spam') / Path('bacon', 'eggs'))
+
+# print(Path.cwd())
+# os.chdir(r'C:\Users\Бегунков Владислав\Documents\repos\auto_pr')
+# print(Path.cwd())
+# print (Path.home())
+
+# dir_test = Path.cwd()
+# print(dir_test)
+# # os.makedirs(r'C:\Users\Бегунков Владислав\Documents\repos\auto_pr\test')
+# Path(r'C:\Users\Бегунков Владислав\Documents\repos\auto_pr\test').mkdir()
+
+##########################################
+
+# file_size = os.path.getsize(r'C:\Users\Бегунков Владислав\Documents\repos\auto_pr\regex\phone_number.py')
+# print(file_size)
+# dir_containts = os.listdir(r'C:\Users\Бегунков Владислав\Documents\repos\auto_pr')
+# print(dir_containts)
+
+totalSize = 0
+for filename in os.listdir(r'C:\Windows\System32'):
+    totalSize = totalSize + os.path.getsize(os.path.join(r'C:\Windows\System32'))
+print(totalSize)
